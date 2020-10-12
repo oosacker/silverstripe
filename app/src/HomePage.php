8b2b802878f4459c4d17dee422ff72517c82dbef
Natsuki Hasegawa <oosacker@gmail.com>
2020-10-12 16:10:25 +1300

    add homepage template

---
+++ app/src/HomePage.php
@@ -0,0 +1,11 @@
+<?php
+
+class HomePage extends Page
+{
+    private static $db = [];
+    private static $has_one = [];
+}
+
+
+
+
---
+++ app/src/HomePageController.php
@@ -0,0 +1,13 @@
+<?php
+
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\HTTPRequest;
+
+class HomePageController extends PageController
+{
+    public function index(HTTPRequest $request) 
+    {
+        if($request)
+            var_dump($request);
+    }
+}
