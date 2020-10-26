ddca2f8daaee41d86d87611dcaa8aca12e41faf7
Natsuki Hasegawa <oosacker@gmail.com>
2020-10-26 23:41:48 +1300

    fix dates

--- app/_config.php
+++ app/_config.php
@@ -3,8 +3,15 @@
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
 use SilverStripe\Akismet\AkismetSpamProtector;
+use SilverStripe\i18n\i18n;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
+
+i18n::set_locale('en_NZ'); 
+
+i18n::config()
+    ->set('date_format', 'dd.MM.yyyy')
+    ->set('time_format', 'HH:mm');
