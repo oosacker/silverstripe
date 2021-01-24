8575872b4606434d0c9bf5aa323e365c591b823a
Natsuki Hasegawa <nat@sunnysideup.co.nz>
2021-01-24 18:38:52 +1300

    investigate routing

--- app/src/Controllers/WeatherCheckPageController.php
+++ app/src/Controllers/WeatherCheckPageController.php
@@ -9,12 +9,33 @@ class WeatherCheckPageController extends PageController
     private static $allowed_actions = [
         'checkweather',
     ];
+
+
+    /*
+    * Routes (see routes.yml): 
+    * /weather/checkweather/12345 - controller/action/ID/OtherID
+    * /weather/ - controller
+    * /check-weather/ - template, can be overridden with index()
+    */
+
+    // public function index() 
+    // {
+    //     $cityId = $this->getRequest()->getVar('ID') ?? '2179538';
+
+    //     echo('<pre>');
+    //     echo($cityId);
+    //     echo('</pre>');
+    // }
     
     public function checkweather()
     {
         $weather = new OpenWeatherConnector();
 
-        $cityId = $this->getRequest()->getVar('city_id') ?? '2179538';
+        $cityId = $this->getRequest()->getVar('ID') ?? '2179538';
+
+        // echo('<pre>');
+        // echo($cityId);
+        // echo('</pre>');
 
         $currentWeather = $weather->getWeather($cityId);
 
---
+++ app/src/Models/MyData.php
@@ -0,0 +1,25 @@
+<?php
+
+use GraphQL\Type\Definition\Type;
+use SilverStripe\GraphQL\TypeCreator;
+use SilverStripe\GraphQL\Pagination\Connection;
+
+class MyData extends TypeCreator
+{
+    public function attributes()
+    {
+        return [
+            'name' => 'member'
+        ];
+    }
+
+    public function fields()
+    {
+        return [
+            'ID' => ['type' => Type::nonNull(Type::id())],
+            'Email' => ['type' => Type::string()],
+            'FirstName' => ['type' => Type::string()],
+            'Surname' => ['type' => Type::string()],
+        ];
+    }
+}
