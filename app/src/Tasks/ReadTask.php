267319a3e4f0e3a06b71a901b2b80092ba7552f3
Natsuki Hasegawa <oosacker@gmail.com>
2021-04-03 12:49:08 +1300

    updated

--- app/src/Tasks/ReadTask.php
+++ app/src/Tasks/ReadTask.php
@@ -15,10 +15,12 @@ class ReadTask extends BuildTask
         echo('reading all rows in TestObject table...<br>');
 
         $list = TestObject::get();
-
+        
+        echo('<hr>');
         foreach($list as $item) {
-            echo $item->Name . ' ' . $item->Number;
-            echo('<br>');
+            
+            echo 'Name: ' . $item->Name . '<br/>Number: ' . $item->Number . '<br/>TestObjectPageID: ' . $item->TestObjectPageID;
+            echo('<hr>');
         }
 
         echo('done with task...<br>');
